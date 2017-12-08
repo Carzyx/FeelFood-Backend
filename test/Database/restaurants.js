e70d4f20770db57fe95a0758193e88452453fbf7
marchilla <miguelarchilla93@gmail.com>
2017-12-08 22:03:34 +0100

    Added restaurant example with images

--- test/Database/restaurants.js
+++ test/Database/restaurants.js
@@ -1,7 +1,16 @@
-    db.restaurants.insert({
-        "username" : "nballelo",
-        "password" : "1234",
-        "name" : "Bulli",
-        "email" : "n.balle.lo@gmail.com",
-        "phone":123456789
-    });
+db.restaurants.insert({
+
+    "_id": "5a2accace4c4178720a6d995",
+    "dishes": [],
+    "email": "n.balle.lo@gmail.com",
+    "images": [
+        "http://www.ecocentro.es/imagenes/contenidos/Restaurante%20vegetariano%20Ecocentro%202.jpg",
+        "https://media.licdn.com/media/AAEAAQAAAAAAAAKAAAAAJDZjOWViYTMxLTkxYjQtNDU2MC05ZTAyLTFlNjgxNjQ0OGI4NQ.jpg",
+        "http://www.sushibo.cat/img/cms/Casanova/Sushibo-safata-w.png",
+        "https://portal.restomontreal.ca/gourmet-burger/gallery/images/a_a_gourmet-burger.jpg"
+    ],
+    "menus": [],
+    "name": "Bulli",
+    "phone": 123456789,
+    "username": "nballelo"
+});
